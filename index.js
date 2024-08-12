551b1ef9e1d4f6e5a36ef8a1e32bdb84c1550633
sxlx333 <knifeq8@gmail.com>
2024-08-12 13:37:18 +0300

    indexjs

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-console.clear();
+/*
+- kintamieji (duomenu tipai):
+    + iniciavimo būdai
+    + number
+    + string (tekstas)
+    + boolean (true/false)
+    + array (sąrašas)
+    + object (kompleksiškas duomuo/struktūra)
+    + null
+    + undefined
+- salygos/palyginimo (if):
+    + boolean logika
+    + if
+    - switch
+    + ternary
+- ciklai (for):
+    + for
+    + for-of
+    + for-in
+    + while
+    - do-while (patys, jei norit)
+    - cikliski metodai:
+        + map
+        + filter
+        - sort
+        - reduce
+        - foreach
+        - N+1...
++ import-export
++ function:
+    + "iprastos" deklaruotinos
+    + anonimines (priskirtos kintamajam)
+    + rodyklines (arrow)
+- methods:
+    + number
+    + string
+    - array
+    - object
+- class (oop):
+    - class
+    - inheritence
+    - private values
+    - static method
+- event listeners:
+    - keyboard click
+    - mouse click (left/main)
+    - mouse click (right/context)
+    - scroll
+*/
 
-console.log(123);
-console.log(4, 5, 6, 7, 8, 9);
 
-
-// console.clear();
-
-let o = 1;
-console.log(o);
-
-o = o + 20;
-console.log(o);
-
-o = o % 6;
-console.log(o);
-
-o = o / 3;
-console.log(o);
+console.log(Array.prototype.flat);
+console.log(Array.prototype.every);
+console.log(Array.prototype.some);
+console.log(Array.prototype.forEach);
+console.log(Array.prototype.sort);
+console.log(Array.prototype.reduce);
